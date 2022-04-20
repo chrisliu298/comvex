9df6c8cfc6450cfd24c6e6488944e4956093a85a
chrisliu298 <yuhaoliu298@gmail.com>
2022-04-19 23:20:17 -0700

    Update

--- collect/train.py
+++ collect/train.py
@@ -26,6 +26,7 @@ def sample_hparams():
     optimizers = ["adam", "sgd"]
     hparams.optimizer = optimizers[np.random.choice(len(optimizers))]
     hparams.lr = np.random.uniform(5e-4, 5e-2)
+    hparams.weight_decay = np.random.uniform(1e-8, 1e-2)
     hparams.dropout_p = np.random.uniform(0, 0.5)
     hparams.initialization = initializations[np.random.choice(len(initializations))]
     return hparams
@@ -39,6 +40,7 @@ def setup(args):
             entity="chrisliu298",
             config=vars(args),
         )
+    np.random.seed(args.seed)
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed_all(args.seed)
 

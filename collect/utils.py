b4eb405d74c76b2990e519870a8f6d7de3084277
chrisliu298 <yuhaoliu298@gmail.com>
2022-04-19 22:33:22 -0700

    Remove unnecessary utils

--- collect/utils.py
+++ collect/utils.py
@@ -1,24 +1,5 @@
 import numpy as np
-import torch
 from easydict import EasyDict
-
-
-def get_weights(parameters):
-    return torch.cat([param.flatten() for param in parameters]).to("cuda")
-
-
-def get_stats(parameters):
-    qs = torch.tensor([0.0, 0.25, 0.5, 0.75, 1.0]).to("cuda")
-    stats = []
-    for param in parameters:
-        param = param.float()
-        stats.append(
-            torch.cat(
-                [torch.mean(param).unsqueeze(0), torch.var(param).unsqueeze(0)]
-            ).to("cuda")
-        )
-        stats.append(torch.quantile(param.flatten(), q=qs).to("cuda"))
-    return torch.cat(stats).to("cuda")
 
 
 def sample_hparams():
